2e3102946a9f8db461134a493a2881ccaa1d9e59
mmvonnseek <maxmullerfitness@gmail.com>
2026-04-14 09:41:33 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+from flask_jwt_extended import JWTManager
+from flask_cors import CORS
+from models import db
+from routes.auth import auth_bp
+from routes.songs import songs_bp
+from routes.playlists import playlists_bp
+
