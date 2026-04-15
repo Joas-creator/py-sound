5b27d9360a516179fa52f3004b04734c1d35fdca
mmvonnseek <maxmullerfitness@gmail.com>
2026-04-15 17:30:32 -0300

    Subindo parte do projeto

--- models.py
+++ models.py
@@ -20,9 +20,41 @@ class User(db.Model):
     playlist = db.relationship('Playlist', backref='owner', lazy=True)
 
     def to_dict(self):
+        return {"id": self.id, "username": self.username, "email": self.email}
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    artist = db.Column(db.String(150), nullable=False)
+    album = db.Column(db.String(150))
+    duration = db.Column(db.Integer)
+    genre = db.Column(db.String(150))
+    url = db.Column(db.String(500))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def to_dict(self):
         return {
             "id": self.id, "title": self.title, "artist": self.artist,
             "album": self.album, "duration": self.duration,
             "genre": self.genre, "url": self.url,
             "added_by": self.owner.username
+        }
+
+class Playlist(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.String(350))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    songs = db.relationship('Song', secondary=playlist_songs, lazy='subquery',
+                            backref=db.backref('playlists', lazy=True))
+    
+    def to_dict(self):
+        return{
+            "id": self.id, "name": self.name,
+            "description": self.description,
+            "owner": self.owner.username,
+            "songs": [s.to_dict() for s in self.songs],
+            "total_songs": len(self.songs)
         }
